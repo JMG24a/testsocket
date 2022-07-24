6be186edce49f5cd34ca5fe1ab57cc084370a9ca
alecap92 <alejandro.cabrejo@gmail.com>
2022-07-24 17:22:41 -0500

    endpoint para mailingList y URI de mongo

---
+++ src/routes/mailingList.js
@@ -0,0 +1,32 @@
+const { Router } = require('express');
+const router = Router();
+const MailingListModel = require('../../database/models/MailingList');
+
+const getMailingList = async (req, res) => {
+  const mailingList = await MailingListModel.find();
+
+  res.status(200).json({
+    msg: 'Listado de Mailing',
+    mailingList,
+  });
+};
+
+const postMailingList = async (req, res) => {
+  const body = req.body;
+  try {
+    const mailList = new MailingListModel(body);
+
+    const newMailing = await mailList.save();
+
+    res.status(201).json({
+      msg: 'Agregado en Mailing List',
+    });
+  } catch {
+    console.log('error');
+  }
+};
+
+router.get('/', getMailingList);
+router.post('/', postMailingList);
+
+module.exports = router;

41b4ccf7b69cd410edb774016d322ce587ae416e
JMG24a <jmg24a@gmail.com>
2022-07-26 10:51:26 -0400

    agregando google

--- auth/index.js
+++ auth/index.js
@@ -1,23 +1,23 @@
 const passport = require('passport');
 const local_auth = require('./strategies/local')
 const JWT_auth = require('./strategies/jwt')
-// const google_auth = require('./strategies/google')
-// const { config } = require('../config/config')
+const google_auth = require('./strategies/google')
+const { config } = require('../config/config')
 
-// const googleKey = config.google.googlePassword;
+const googleKey = config.google.googlePassword;
 
 passport.use(local_auth)
 passport.use(JWT_auth)
-// passport.use(google_auth)
+passport.use(google_auth)
 
-// function passport_sessions (app){
-//   app.use(session({
-//     secret: googleKey,
-//     resave: false,
-//     saveUninitialized: false
-//   }))
-//   app.use(passport.initialize())
-//   app.use(passport.session())
-// }
+function passport_sessions (app){
+  app.use(session({
+    secret: googleKey,
+    resave: false,
+    saveUninitialized: false
+  }))
+  app.use(passport.initialize())
+  app.use(passport.session())
+}
 
-// module.exports = {passport_sessions}
+module.exports = {passport_sessions}

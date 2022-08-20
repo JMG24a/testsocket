b7835ccf1e197a8d853014f709e40d3795940bb0
alecap92 <alejandro.cabrejo@gmail.com>
2022-08-20 09:52:40 -0500

    update

--- models/Product.js
+++ models/Product.js
@@ -1,19 +1,19 @@
-const { Schema, model, models } = require("mongoose");
+const { Schema, model, models } = require('mongoose');
 
 const productSchema = new Schema({
-  name           : { type: String },
-  updatedDate    : { type: String },
-  priceList      : { type: Number },
-  description    : [String],
-  availablePromos: [{ type: Schema.Types.ObjectId, ref: 'Promotion' }],
+  name: { type: String },
+  updatedDate: { type: String },
+  priceList: { type: Number },
+  description: [String],
+  availablePromos: { type: String },
 });
 
-productSchema.method("toJSON", function () {
+productSchema.method('toJSON', function () {
   const { __v, _id, ...object } = this.toObject();
   object.id = _id;
   return object;
 });
 
-const Product = models.Product || model("Product", productSchema);
+const Product = models.Product || model('Product', productSchema);
 
 module.exports = Product;

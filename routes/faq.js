249f63cacfda69fea04983944953b67495ee1a5f
JMG24a <jmg24a@gmail.com>
2022-10-01 00:59:41 -0400

    crud para FAQ y PLANS

--- routes/faq.js
+++ routes/faq.js
@@ -18,7 +18,40 @@ const postFaq = async (req, res) => {
     const newMailing = await faqList.save();
 
     res.status(201).json({
+      ok: true,
       msg: 'Agregado en FAQ List',
+      response: newMailing
+    });
+  } catch {
+    console.log('error');
+  }
+};
+
+const putFaq = async (req, res) => {
+  const {id} = req.params;
+  const body = req.body;
+  try {
+    const updateFAQ = await FaqModel.findByIdAndUpdate(id, body, { new: true })
+
+    res.status(201).json({
+      ok: true,
+      msg: 'FAQ Actializado',
+      result: updateFAQ
+    });
+  } catch {
+    console.log('error');
+  }
+};
+
+const delFaq = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteFAQ = await FaqModel.findByIdAndDelete(id)
+
+    res.status(201).json({
+      ok: true,
+      msg: 'FAQ Eliminado',
+      result: deleteFAQ
     });
   } catch {
     console.log('error');
@@ -27,5 +60,7 @@ const postFaq = async (req, res) => {
 
 router.get('/', getFaq);
 router.post('/', postFaq);
+router.put('/:id', putFaq);
+router.delete('/:id', delFaq);
 
 module.exports = router;
--- routes/product.js
+++ routes/product.js
@@ -18,12 +18,46 @@ const postProducts = async (req, res) => {
   await newProduct.save();
 
   res.status(201).json({
+    ok: true,
     msg: 'Producto creado exitosamente',
     product: newProduct
   });
 };
 
+const putProducts = async (req, res) => {
+  const {id} = req.params;
+  const body = req.body;
+  try {
+    const updateProduct = await Product.findByIdAndUpdate(id, body, { new: true })
+
+    res.status(201).json({
+      ok: true,
+      msg: 'Products Actializado',
+      result: updateProduct
+    });
+  } catch {
+    console.log('error');
+  }
+};
+
+const delProducts = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(id)
+
+    res.status(201).json({
+      ok: true,
+      msg: 'Products Eliminado',
+      result: deleteProduct
+    });
+  } catch {
+    console.log('error');
+  }
+};
+
 router.get('/', getProducts);
 router.post('/', postProducts);
+router.put('/:id', putProducts);
+router.delete('/:id', delProducts);
 
 module.exports = router;

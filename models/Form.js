7c4adea03eb4165a9763ea009cc7e5c6cad0619e
alecap92 <alejandro.cabrejo@gmail.com>
2022-10-14 11:25:37 -0500

    add meta to model Form

--- models/Form.js
+++ models/Form.js
@@ -2,27 +2,32 @@ const { Schema, model, models } = require('mongoose');
 
 const formSchema = new Schema(
   {
-    appAvailable : { type: Boolean },
-    webAvailable : { type: Boolean },
-    title        : { type: String },
-    description  : { type: String },
-    abstract     : { type: String },
-    image        : { type: String },
-    keywords     : { type: String },
-    webPostOwner : { type: String },
-    slug         : { type: String },
-    tag          : { type: String },
-    type         : { type: String, enum: ['tramite', 'formato'] },
-    requiredForms: [{
-      name    : { type: String },
-      fileName: { type: String }
-    }],
+    appAvailable: { type: Boolean },
+    webAvailable: { type: Boolean },
+    title: { type: String },
+    description: { type: String },
+    abstract: { type: String },
+    image: { type: String },
+    keywords: { type: String },
+    webPostOwner: { type: String },
+    slug: { type: String },
+    tag: { type: String },
+    metaTitle: { type: String },
+    metaDescription: { type: String },
+    metaUrl: { type: String },
+    type: { type: String, enum: ['tramite', 'formato'] },
+    requiredForms: [
+      {
+        name: { type: String },
+        fileName: { type: String },
+      },
+    ],
     webContentPost: { type: Schema.Types.ObjectId, ref: 'Article' },
     formCategory: { type: Schema.Types.ObjectId, ref: 'FormCategory' },
     formData: [
       {
-        stepName   : { type: String },
-        completed  : { type: Boolean },
+        stepName: { type: String },
+        completed: { type: Boolean },
         globalAlert: { type: String },
         formDiagram: {
           title: { type: String },
@@ -49,7 +54,7 @@ const formSchema = new Schema(
                 type: { type: String },
                 options: [String],
                 isRequired: Boolean,
-              }
+              },
             },
           ],
         },

cb6f897b172a3482ec2ed7d093a5fb8cf720ef61
alecap92 <alejandro.cabrejo@gmail.com>
2022-11-14 13:21:39 -0500

    upd

--- models/company.js
+++ models/company.js
@@ -1,133 +1,135 @@
-const { ObjectId } = require("mongodb");
-const { Schema, model } = require("mongoose");
+const { ObjectId } = require('mongodb');
+const { Schema, model } = require('mongoose');
 
-const companySchema = Schema({
-  name: { type: String },
-  email: { type: String },
-  nit: { type: String },
-  logo: { type: String },
-  mobile: { type: String },
-  userId: [{ type: Schema.Types.ObjectId, ref: 'User'}],
-  employeesId:[{ type: Schema.Types.ObjectId, ref: 'User' }],
-  socialNetworks:{
-    webPage: { type: String },
-    facebookUrl: { type: String },
-    linkedinUrl: { type: String },
-    instagramUrl: { type: String },
-    twitterUrl: { type: String },
+const companySchema = Schema(
+  {
+    name: { type: String },
+    email: { type: String },
+    nit: { type: String },
+    logo: { type: String },
+    mobile: { type: String },
+    userId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    employeesId: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+    socialNetworks: {
+      webPage: { type: String },
+      facebookUrl: { type: String },
+      linkedinUrl: { type: String },
+      instagramUrl: { type: String },
+      twitterUrl: { type: String },
+    },
+    location: {
+      address: { type: String },
+      city: { type: String },
+      department: { type: String },
+    },
+    paymentMethod: [
+      '100% anticipado',
+      '50% anticipao y 50% contraentrega',
+      'Credito 15 dias',
+      'Credito 30 dias',
+    ],
+    corporativeColors: {
+      primary: { type: String },
+      secondary: { type: String },
+    },
+    templates: {
+      quotation: { type: String },
+      invoice: { type: String },
+    },
+    workOrders: [
+      {
+        id: { type: ObjectId },
+        date: { type: String },
+        contact: { type: Schema.Types.ObjectId, ref: 'Relation' },
+        status: { type: String },
+        importation: { type: String },
+        shippingDate: { type: String },
+        DateOfReceipt: { type: String },
+        observations: { type: String },
+      },
+    ],
+    sales: [
+      {
+        date: { type: String },
+        contact: { type: Schema.Types.ObjectId, ref: 'Relation' },
+        paymentMethod: { type: String },
+        seller: { type: String },
+        products: [],
+        observations: { type: String },
+        subTotal: { type: String },
+        valueIva: { type: String },
+        sendValue: { type: String },
+        total: { type: String },
+      },
+    ],
+    quotations: [
+      {
+        date: { type: String },
+        contact: { type: Schema.Types.ObjectId, ref: 'Relation' },
+        paymentMethod: { type: String },
+        seller: { type: String },
+        products: [],
+        observations: { type: String },
+        subTotal: { type: String },
+        valueIva: { type: String },
+        sendValue: { type: String },
+        total: { type: String },
+      },
+    ],
+    products: [
+      {
+        name: { type: String },
+        description: { type: String },
+        price: { type: String },
+        image: { type: String },
+      },
+    ],
+    account: {
+      contactId: { type: Schema.Types.ObjectId, ref: 'Relation' },
+      accountName: { type: String },
+      accountId: { type: ObjectId },
+      type: { type: String, enum: ['Vendedor', 'Cliente', 'partner'] },
+      priority: { type: String, enum: ['Vendedor', 'Cliente', 'partner'] },
+      category: { type: String },
+      subCategory: { type: String },
+      email: { type: String },
+      accountValue: { type: String },
+      website: { type: String },
+      observations: { type: String },
+      source: { type: String },
+      city: { type: String },
+      address: { type: String },
+      country: { type: String },
+      state: { type: String },
+      nit: { type: String },
+      stage: { type: String },
+      phone: { type: String },
+    },
+    opportunities: {
+      dealName: { type: String },
+      dealValue: { type: String },
+      closeProbability: { type: String },
+      expiredCloseDate: { type: String },
+      dealCreateDate: { type: String },
+      notes: { type: String },
+      accountId: { type: ObjectId },
+      contactId: { type: Schema.Types.ObjectId, ref: 'Relation' },
+      stage: { type: String },
+      priority: { type: String },
+      forecastValue: { type: String },
+      ActualDealValue: { type: String },
+      dealLength: { type: String },
+    },
   },
-  location: {
-    address: { type: String },
-    city: { type: String },
-    department: { type: String },
-  },
-  paymentMethod: ["100% anticipado", "50% anticipao y 50% contraentrega", "Credito 15 dias", "Credito 30 dias"],
-  corporativeColors: {
-    primary: {type: String },
-    secondary: {type: String },
-  },
-  templates: {
-    quotation: {type: String },
-    invoice: {type: String}
-  },
-  workOrders:[{
-    id: {type: ObjectId},
-    date: {type: String},
-    contact: { type: Schema.Types.ObjectId, ref: 'Relation'},
-    status: {type: String},
-    importation: {type: String},
-    shippingDate: {type: String},
-    DateOfReceipt: {type: String},
-    observations: {type: String}
-  }],
-  sales: [{
-    date: {type: String},
-    contact: { type: Schema.Types.ObjectId, ref: 'Relation'},
-    paymentMethod: {type: String},
-    seller: {type: String},
-    products: [],
-    observations: {type: String},
-    subTotal: {type: String},
-    valueIva: {type: String},
-    sendValue: {type: String},
-    total: {type: String},
-  }],
-  quotations: [{
-    date: {type: String},
-    contact: { type: Schema.Types.ObjectId, ref: 'Relation'},
-    paymentMethod: {type: String},
-    seller: {type: String},
-    products: [],
-    observations: {type: String},
-    subTotal: {type: String},
-    valueIva: {type: String},
-    sendValue: {type: String},
-    total: {type: String},
-  }],
-  products: [{
-    name: {type: String},
-    description: {type: String},
-    price: {type: String},
-    image: {type: String}
-  }],
-  account:{
-    contactId: { type: Schema.Types.ObjectId, ref: 'Relation' },
-    accountName: {type: String},
-    accountId: {type: ObjectId},
-    type: {type: String, enum: ['Vendedor', 'Cliente', 'partner']},
-    priority: {type: String, enum: ['Vendedor', 'Cliente', 'partner']},
-    category: {type: String},
-    subCategory: {type: String},
-    email: {type: String},
-    accountValue: {type: String},
-    website: {type: String},
-    commentaries: {type: String},
-    source: {type: String},
-    city: {type: String},
-    address: {type: String},
-    country: {type: String},
-    nit: {type: String},
-    stage: {type: String}
-  },
-  opportunities: {
-    dealName: {type: String},
-    dealValue: {type: String},
-    closeProbability: {type: String},
-    expiredCloseDate: {type: String},
-    dealCreateDate: {type: String},
-    notes: {type: String},
-    accountId: {type: ObjectId},
-    contactId: { type: Schema.Types.ObjectId, ref: 'Relation' },
-    stage: {type: String},
-    priority: {type: String},
-    forecastValue: {type: String},
-    ActualDealValue: {type: String},
-    dealLength: {type: String},
+  {
+    timestamps: true,
   }
-},
-{
-  timestamps: true,
-});
+);
 
-companySchema.method("toJSON", function () {
+companySchema.method('toJSON', function () {
   const { __v, _id, ...object } = this.toObject();
   object.id = _id;
   return object;
 });
 
-module.exports = model("Company", companySchema);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+module.exports = model('Company', companySchema);

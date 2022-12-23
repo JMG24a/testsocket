63893add4601c23a1ab11e678ad794c1622cea64
alecap92 <alejandro.cabrejo@gmail.com>
2022-12-22 20:14:04 -0500

    more templates

--- services/files.js
+++ services/files.js
@@ -1,11 +1,11 @@
 const { response } = require('express');
-const fs = require("fs")
-const path = require("path")
-const { documentWithHtmlAndCss } = require("../templates")
-const { pdfEmail } = require("../mails/pdfEmail.js")
+const fs = require('fs');
+const path = require('path');
+const { documentWithHtmlAndCss } = require('../templates');
+const { pdfEmail } = require('../mails/pdfEmail.js');
 //Lib
-const pdf = require("pdf-creator-node")
-const Handlebars = require("handlebars");
+const pdf = require('pdf-creator-node');
+const Handlebars = require('handlebars');
 const HTMLtoDOCX = require('html-to-docx');
 const boom = require('@hapi/boom');
 
@@ -13,136 +13,141 @@ const boom = require('@hapi/boom');
 const { formatProcedureStages } = require('../helper/procedureStages');
 
 generatePDFFile = (data, nameFile, res = response) => {
-  const { procedure, options : userOptions } = data;
-  if(!procedure){
-    throw boom.badData('La información necesaria está incompleta')
+  const { procedure, options: userOptions } = data;
+  if (!procedure) {
+    throw boom.badData('La información necesaria está incompleta');
   }
 
   const FILE_NAME = nameFile;
-  const html = documentWithHtmlAndCss(FILE_NAME)
+  const html = documentWithHtmlAndCss(FILE_NAME);
 
   const options = {
     ...userOptions,
-    border: "20mm",
+    border: '15mm',
     // header: {
     //   height: "45mm",
     //   contents: '<div style="text-align: right; color: gray;">Realizado con <b>@FormuApp</b></div>'
     // },
     footer: {
-      height: "10mm",
+      height: '1mm',
       contents: {
         // first: 'Cover page',
         // 2: 'Second page', // Any page number is working. 1-based index
-        default: '<div style="text-align: right; color: gray;">Realizado con <b>@FormuApp</b></div>', // fallback value
+        default:
+          '<div style="text-align: right; color: gray;">Realizado con <b>@FormuApp</b></div>', // fallback value
         // last: 'Last Page'
-      }
-    }
-  }
+      },
+    },
+  };
 
-  console.log("Procedure stages: ", formatProcedureStages(procedure.stages))
+  console.log('Procedure stages: ', formatProcedureStages(procedure.stages));
 
   const document = {
     html: html,
     data: formatProcedureStages(procedure.stages) || {},
-    path: "./output.pdf",
-    type: "buffer",
-  }
+    path: './output.pdf',
+    type: 'buffer',
+  };
 
-  pdf.create(document, options)
-  .then((result) => {
+  pdf
+    .create(document, options)
+    .then((result) => {
       res.setHeader('Content-Type', 'application/pdf');
-      res.setHeader('Content-Disposition', `attachment; filename=${FILE_NAME}.pdf`);
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=${FILE_NAME}.pdf`
+      );
       res.send(result);
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
       console.error(error);
-  });
-}
+    });
+};
 
-generateEmailFile = async(data, nameFile, res = response) => {
+generateEmailFile = async (data, nameFile, res = response) => {
   const { email, procedure } = data;
-  if(!email | !procedure){
-    throw boom.badData('La información necesaria está incompleta')
+  if (!email | !procedure) {
+    throw boom.badData('La información necesaria está incompleta');
   }
 
-  const FILE_NAME = nameFile
-  const html = documentWithHtmlAndCss(FILE_NAME)
+  const FILE_NAME = nameFile;
+  const html = documentWithHtmlAndCss(FILE_NAME);
 
   const options = {
-    format: "Letter",
-    orientation: "portrait",
-    border: "20mm",
-  }
+    format: 'Letter',
+    orientation: 'portrait',
+    border: '20mm',
+  };
 
   const document = {
     html: html,
     data: formatProcedureStages(procedure.stages) || {},
-    path: "./output.pdf",
-    type: "buffer",
-  }
+    path: './output.pdf',
+    type: 'buffer',
+  };
 
-  try{
-    const result = await pdf.create(document, options)
+  try {
+    const result = await pdf.create(document, options);
     const content = `<b>Formuapp</b>`;
     await pdfEmail(
-      email, 
-      procedure.idUsers[0], 
-      content, 
-      result, 
+      email,
+      procedure.idUsers[0],
+      content,
+      result,
       procedure.title
     );
     res.json({
       ok: true,
-      msg: "correo enviado"
+      msg: 'correo enviado',
     });
-  }catch(e){
-    throw boom.badData("No se logro enviar el correo")
+  } catch (e) {
+    throw boom.badData('No se logro enviar el correo');
   }
-}
+};
 
 generateXLSFile = (procedure, nameFile, res = response) => {
-    //Toda la lógica necesaria para generar un archivo xls y retornarlo
+  //Toda la lógica necesaria para generar un archivo xls y retornarlo
 
-    return res.status(200).json({
-        ok: true,
-        file: "Archivo XLS generado"
-    });
-}
+  return res.status(200).json({
+    ok: true,
+    file: 'Archivo XLS generado',
+  });
+};
 
 generateZIPFile = (procedure, nameFile, res = response) => {
-    //Toda la lógica necesaria para generar un archivo zip y retornarlo
+  //Toda la lógica necesaria para generar un archivo zip y retornarlo
 
-    return res.status(200).json({
-        ok: true,
-        file: "Archivo ZIP generado"
-    });
-}
+  return res.status(200).json({
+    ok: true,
+    file: 'Archivo ZIP generado',
+  });
+};
 
-generateDOCFile = async(data, nameFile, res = response) => {
-  const { procedure } = data
-  if(!procedure){
-    throw boom.badData('La información necesaria está incompleta')
+generateDOCFile = async (data, nameFile, res = response) => {
+  const { procedure } = data;
+  if (!procedure) {
+    throw boom.badData('La información necesaria está incompleta');
   }
 
-  const FILE_NAME = nameFile
-  let preHtml = documentWithHtmlAndCss(FILE_NAME)
+  const FILE_NAME = nameFile;
+  let preHtml = documentWithHtmlAndCss(FILE_NAME);
 
   html = Handlebars.compile(preHtml)({
-    data: formatProcedureStages(procedure.stages) || {}
+    data: formatProcedureStages(procedure.stages) || {},
   });
   const fileBuffer = await HTMLtoDOCX(html, null);
   res.setHeader('Content-Type', 'application/docx');
   res.setHeader('Content-Disposition', 'attachment; filename=download.docx');
   res.send(fileBuffer);
-}
+};
 
 generateJPGFile = (procedure, nameFile, res = response) => {
   //Toda la lógica necesaria para generar un archivo jpg y retornarlo
   return res.status(200).json({
     ok: true,
-    file: "Archivo JPG generado"
+    file: 'Archivo JPG generado',
   });
-}
+};
 
 module.exports = {
   generatePDFFile,
@@ -150,5 +155,5 @@ module.exports = {
   generateXLSFile,
   generateZIPFile,
   generateDOCFile,
-  generateJPGFile
-}
+  generateJPGFile,
+};
